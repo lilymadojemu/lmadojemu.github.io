e99a926f6b6f614988116202085717715e435afb
lilymadojemu <70119291+lilymadojemu@users.noreply.github.com>
2023-11-20 20:38:15 +0900

    Updates

---
+++ assets/js/projectDetail.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const projectDetailInfo = {
+        "Portfolio":{},
+        "Let's Go to Japan": {},
+        "TravelTagger":{},
+        "Let's Learn Hiragana":{},
+        "Research Paper: Jane's Language Learning":{},
+        "Data Story":{},
+    }
+});
